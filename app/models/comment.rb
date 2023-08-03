06cea675c3d887e3d4ef504b2951a49779c2366a
notarajesh1110 <rajesh.runiwal@1mg.com>
2023-08-03 12:17:16 +0530

    dummy brANCH

--- app/models/comment.rb
+++
@@ -1,8 +0,0 @@
-class Comment < ApplicationRecord
-
-  belongs_to :post
-  belongs_to :parent_comment, class_name: 'Comment', optional: true
-  has_many :replies, class_name: 'Comment', foreign_key: :parent_comment_id, dependent: :destroy
-
-  validates :body, length: { minimum: 5}
-end
--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-
-  root "posts#index"
-  resources :posts do
-    resources :comments
-  end
-  # other routes should be present as well
-end
